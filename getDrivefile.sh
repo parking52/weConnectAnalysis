62498c6169cbae63af19a1b963de9848723a7fe7
Melchior Fracas <melchior.fracas@gmail.com>
2015-10-24 14:35:23 +0200

    stuff for starting

---
+++ getDrivefile.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YsaTXnp2KlGf1ZiHNWQTI-uYu-U49bY6oGVFV0uv0Co' -O survey_result
+
+
---
+++ notebook.py
@@ -0,0 +1,14 @@
+__author__ = 'melchior'
+
+import pandas as pd
+
+
+working_data_frame = pd.read_csv('value.txt')
+
+print(working_data_frame)
+
+
+
+
+
+
